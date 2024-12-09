033e2b31f8455189d289615c47eaa2bf6f549ba3
Adam Checinski <achecinski@clari.com>
2024-12-09 22:13:33 +0100

    day 5

---
+++ src/main/kotlin/Day5.kt
@@ -0,0 +1,64 @@
+import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
+import java.io.File
+
+fun day5part1(input: List<String>): Int {
+    val delimiter = ""
+    val index = input.indexOf(delimiter)
+
+    val beforeRules = mutableMapOf<Int, List<Int>>()
+    val afterRules = mutableMapOf<Int, List<Int>>()
+
+    input.take(index).map { it.split('|') }.forEach {
+        val before = it[0].toInt()
+        val after = it[1].toInt()
+        afterRules[before] = afterRules.getOrDefault(before, emptyList()) + after
+        beforeRules[after] = beforeRules.getOrDefault(after, emptyList()) + before
+    }
+
+    val pagesToProduce = input.drop(index + 1).map { it.split(',').map { e -> e.toInt() }.toList() }
+
+    var sum = 0
+
+    pagesToProduce.forEach {
+        var pageRulesAreCorrect = true
+        for (i in it.indices) {
+            val beforeNumbers = it.take(i)
+            val afterNumbers = it.drop(i + 1)
+            val verifiedNumber = it[i]
+
+            val beforeRulesForNumber = beforeRules.getOrDefault(verifiedNumber, emptyList())
+            val afterRulesForNumber = afterRules.getOrDefault(verifiedNumber, emptyList())
+
+            if (beforeRulesForNumber.any { afterNumbers.contains(it)} || afterRulesForNumber.any { beforeNumbers.contains(it)}) {
+                pageRulesAreCorrect = false
+                break
+            }
+        }
+        if (pageRulesAreCorrect) {
+            sum += it[it.size/2]
+        }
+    }
+
+    return sum
+}
+
+class Day5Test {
+    private val inputFile = File(javaClass.classLoader.getResource("day5input").file)
+    private val exampleInputFile = File(javaClass.classLoader.getResource("day5exampleInput").file)
+    private val part1ExampleAnswer = 143
+    private val part1Answer = 4578
+
+    @Test
+    fun `part 1 answer check`() {
+        val result = day5part1(inputFile.readLines())
+        expectThat(result).isEqualTo(part1Answer)
+    }
+
+    @Test
+    fun `part 1 example answer check`() {
+        val result = day5part1(exampleInputFile.readLines())
+        expectThat(result).isEqualTo(part1ExampleAnswer)
+    }
+}
