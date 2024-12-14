7e3b670cb69a977c3e0ebcbeaa30056619706301
Adam Checinski <achecinski@clari.com>
2024-12-14 18:02:42 +0100

    day 7 part 2

--- src/main/kotlin/Day7.kt
+++ src/main/kotlin/Day7.kt
@@ -7,6 +7,29 @@ import strikt.assertions.isEqualTo
 import java.io.File
 
 fun day7part1(input: List<String>): Long {
+    val operations: List<(Long, Long) -> Long> = listOf(
+        { a, b -> a + b },
+        { a, b -> a * b }
+    )
+
+    return recalibrateUsingOperations(input, operations)
+}
+
+
+fun day7part2(input: List<String>): Long {
+    val operations: List<(Long, Long) -> Long> = listOf(
+        { a, b -> a + b },
+        { a, b -> a * b },
+        { a, b -> (a.toString() + b.toString()).toLong() }
+    )
+
+    return recalibrateUsingOperations(input, operations)
+}
+
+private fun recalibrateUsingOperations(
+    input: List<String>,
+    operations: List<(Long, Long) -> Long>
+): Long {
     val resultToNumbers = input.map { line ->
         val (value, numbers) = line.split(":").map { it.trim() }
         value.toLong() to numbers.split(" ").map { it.toLong() }
@@ -14,7 +37,7 @@ fun day7part1(input: List<String>): Long {
 
     var sum = 0L
     for ((expectedResult, numbers) in resultToNumbers) {
-        val result = calculateIterative(numbers, expectedResult)
+        val result = calculateIterativeWithOps(numbers, expectedResult, operations)
         if (result == expectedResult) {
             sum += expectedResult
         }
@@ -22,7 +45,7 @@ fun day7part1(input: List<String>): Long {
     return sum
 }
 
-fun calculateIterative(numbers: List<Long>, expectedResult: Long): Long {
+fun calculateIterativeWithOps(numbers: List<Long>, expectedResult: Long, operations: List<(Long, Long) -> Long>): Long {
     if (numbers.size < 2) {
         return if (numbers.size == 1 && numbers[0] == expectedResult) expectedResult else 0
     }
@@ -33,8 +56,9 @@ fun calculateIterative(numbers: List<Long>, expectedResult: Long): Long {
 
     val stack = mutableListOf<Pair<Long, List<Long>>>()
 
-    stack.add((first + second) to remainder)
-    stack.add((first * second) to remainder)
+    for (op in operations) {
+        stack.add(op(first, second) to remainder)
+    }
 
     while (stack.isNotEmpty()) {
         val (currentResult, remaining) = stack.removeAt(stack.size - 1)
@@ -46,16 +70,12 @@ fun calculateIterative(numbers: List<Long>, expectedResult: Long): Long {
         } else {
             val next = remaining[0]
             val nextRemaining = remaining.drop(1)
-
-            stack.add((currentResult + next) to nextRemaining)
-            stack.add((currentResult * next) to nextRemaining)
+            for (operation in operations) {
+                stack.add(operation(currentResult, next) to nextRemaining)
+            }
         }
     }
 
-    return 0
-}
-
-fun day7part2(input: List<String>): Int {
     return 0
 }
 
@@ -64,9 +84,9 @@ class Day7Test {
     private val inputFile = File(javaClass.classLoader.getResource("day7input").file)
     private val exampleInputFile = File(javaClass.classLoader.getResource("day7exampleInput").file)
     private val part1ExampleAnswer = 3749L
-    private val part2ExampleAnswer = 0L
+    private val part2ExampleAnswer = 11387L
     private val part1Answer = 303876485655L
-    private val part2Answer = 0
+    private val part2Answer = 146111650210682L
 
     @Test
     fun `part 1 example answer check`() = runTest {
@@ -82,13 +102,13 @@ class Day7Test {
 
     @Test
     fun `part 2 answer check`() = runTest {
-//        val result = day7part2(inputFile.readLines())
-//        expectThat(result).isEqualTo(part2Answer)
+        val result = day7part2(inputFile.readLines())
+        expectThat(result).isEqualTo(part2Answer)
     }
 
     @Test
     fun `part 2 example answer check`() = runTest {
-//        val result = day7part2(exampleInputFile.readLines())
-//        expectThat(result).isEqualTo(part2ExampleAnswer)
+        val result = day7part2(exampleInputFile.readLines())
+        expectThat(result).isEqualTo(part2ExampleAnswer)
     }
 }
